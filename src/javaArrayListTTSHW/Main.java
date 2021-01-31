816884e032418303a5fdab5d460afac4f8ffee81
Joey Whalen <jtktwhalen@gmail.com>
2021-01-30 19:47:40 -0500

    update Main

--- src/javaArrayListTTSHW/Main.java
+++ src/javaArrayListTTSHW/Main.java
@@ -3,7 +3,8 @@ package javaArrayListTTSHW;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Ask user for 5 numbers, store them in an ArrayList, and use numbers for math
+		
 
 	}
 
